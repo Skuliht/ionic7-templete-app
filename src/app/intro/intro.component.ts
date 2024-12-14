ff4b4a3702620ade4877d96187da3f75fc1ce3b5
Your Name <you@example.com>
2024-12-14 18:38:44 +0300

    feat: add books table

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book{
+  b_avtor:string,
+  b_nazv: string
+}
+
+
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent  implements OnInit {
-
+  
+  books: Book[] = [
+    {
+      b_avtor:"Лопухов",
+      b_nazv:"Лопухи"
+    }, 
+    {
+      b_avtor:"Собакин",
+      b_nazv:"Собаки"
+    }, 
+    {
+      b_avtor:"Ласточкин",
+      b_nazv:"Ласточки"
+    }
+  ]  
   constructor() { }
 
   ngOnInit() {}
